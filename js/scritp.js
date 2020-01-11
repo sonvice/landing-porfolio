3d12e2793df45461f9a02c04275d992e98cf285d
wilsonelwilson <wvmc_es@hotmail.com>
2020-01-11 16:55:01 -0500

    creando la funcion del menu toggle, quitar y añadir clase

---
+++ js/scritp.js
@@ -0,0 +1,5 @@
+
+let toggle = document.querySelector(".menu-toggle").addEventListener("click", function(){
+    let menu = document.querySelector(".nav").classList.toggle("active")
+})
+console.log(toggle)
