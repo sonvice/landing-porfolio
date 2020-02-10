fe463b3c86c0353b974cca8753fb9e0fcd5f90e9
wilsonelwilson <wvmc_es@hotmail.com>
2020-02-10 17:13:15 -0500

    Creando evento click a la galería

--- js/scritp.js
+++ js/scritp.js
@@ -2,4 +2,28 @@
 let toggle = document.querySelector(".menu-toggle").addEventListener("click", function(){
     let menu = document.querySelector(".nav").classList.toggle("active")
 })
-console.log(toggle)
+
+/* ----- RECORRIENDO GALERÍA ----- */
+
+// let img = document.querySelectorAll('.grid');
+
+// let  nodeArray = [].slice.call(img);
+
+// let hijoNodo = nodeArray[0].children[1]
+
+// console.log(hijoNodo.getElementByclassName('grid__figure'))
+
+
+let galerery = document.getElementById("grid").children;
+let arrayGalery = Array.prototype.slice.call(galerery)
+
+for (let i = 0; i < arrayGalery.length; i++) {
+    const galery = arrayGalery[i];
+
+    galery.addEventListener("click", function(){
+        galery.classList.toggle("active")
+    })
+
+   console.log()
+    
+}
